26eb184e3108750696744415116963fe93fd7bac
arpitparekh <arpitparekh9@gmail.com>
2024-09-27 09:17:46 +0530

    list items

--- first_project/screen_one/views.py
+++ first_project/screen_one/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 def show_text(request):    # functional views
   context = {
     'text': 'Hello World',
+    'fruits' : ['Apple','Banana','Cherry','Mango','Dragon Fruit']
   }
+
   return render(request,"show_text.html",context)
