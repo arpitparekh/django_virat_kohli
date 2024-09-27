cc9fb08ee2738105d64801e9560af9a09285a47a
arpitparekh <arpitparekh9@gmail.com>
2024-09-27 11:44:47 +0530

    contact_form

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'first_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'atul_batch',
+        'USER':'root',
+        'PASSWORD':'Walden0042$$',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
