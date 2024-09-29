5923fface2378fc98e0838943d28524bda86d609
arpitparekh <arpitparekh9@gmail.com>
2024-09-29 14:06:02 +0530

    screen_update

--- first_project/screen_one/urls.py
+++ first_project/screen_one/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.show_dashboard, name='dashboard'),
     path('pass_data/', views.show_pass_data, name='pass_data'),
+    path('mysql_database/', views.mysql_database, name='mysql_database'),
 ]
--- first_project/screen_one/views.py
+++ first_project/screen_one/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
+class User:
+    def __init__(self,name,age,gender,company,image):
+        self.image = image
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.company = company
+
+
 def show_dashboard(request):
     return render(request, "dashboard.html")
 
+def mysql_database(request):
+    return render(request, "my_sql_database.html")
+
 def show_pass_data(request):
+
+    user = User("Atul",22,"Male","Bascom","https://images.pexels.com/photos/5593326/pexels-photo-5593326.jpeg?auto=compress&cs=tinysrgb&w=600")
+
     context = {
-        'data': 'This is passed data'
+        'data': 'This is passed data',
+        'fruits' : ["Banana","Apple","Watermelon","Pomegranade","Dates","Guvava","Kiwi"],
+        'user':user,
     }
     return render(request, "pass_data.html", context)
